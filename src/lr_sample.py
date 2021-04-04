c04fe0d11747115ca12ef0d58e438ba5d35b5213
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2021-04-04 21:26:55 +0900

    update lr_sample.py

--- src/lr_sample.py
+++ src/lr_sample.py
@@ -1,40 +1,97 @@
+import argparse
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 
-win_radius = 25
+def check_args(args):
+    if args.train is None:
+        print(parser.print_help())
+        exit(1)
+    if args.test is not None and args.out is None:
+        print(parser.print_help())
+        exit(1)
 
-n = 0
-_data = []
-label = []
-
-for line in open("train.dat").readlines():
-    if n%2 == 0:
-        seq = line.rstrip("\n")            
-    else:
-        ss  = line.rstrip("\n")
-
-        m = 0
-        seq = ("_" * win_radius) + seq + ("_" * win_radius) #add spacer
-        for _label in ss:
-            label.append(int(_label))
-            _in = list(seq[m:m+win_radius*2+1])
+def generate_input(df, window_radius=1):
+    _data = []
+    for _, item in df.iterrows():
+        seq = item.sequence
+        length = len(seq)
+        
+        seq = ("_" * window_radius) + seq + ("_" * window_radius) #add spacer
+        for resn in range(length):
+            _in = list(seq[resn:resn+window_radius*2+1])
             _data.append(_in)
-            m += 1
+    return _data
 
-    n += 1
+def generate_label(df):
+    label = []
+    for _, item in df.iterrows():
+        ss = item.label
+        for resn, _label in enumerate(ss):
+            label.append(int(_label))
+    return label
 
-transformer = OneHotEncoder().fit(_data)
-dst = transformer.transform(_data)
-data = dst.toarray()
-print(data.shape)
+if __name__ == "__main__":
+    
 
-X_train, X_test, y_train, y_test = train_test_split(data, label, random_state=0)
+    parser = argparse.ArgumentParser(description="example program")
+    parser.add_argument("-train", help="path to training data (required)")
+    parser.add_argument("-test", help="path to test data (optional)")
+    parser.add_argument("-out", help="path to predicted information for test data (required only if --test is set)")
+    parser.add_argument("--window_radius", type=int, default=1)
+    args = parser.parse_args()
 
-model = LogisticRegression(max_iter=1000).fit(X_train, y_train)
-score = model.score(X_test, y_test)
-print('Q2 accuracy: %.4f'%(score))
+    check_args(args)
+    
+    train_df = pd.read_csv(args.train)
 
-auc = roc_auc_score(y_test, model.predict(X_test))
-print('AUC: %.4f'%(auc))
+    _data = []
+    label = []
+
+    window_radius = args.window_radius
+    _data = generate_input(train_df, window_radius)
+    label = generate_label(train_df)
+
+    transformer = OneHotEncoder().fit(_data)
+    data = transformer.transform(_data)
+    label = np.array(label)
+
+
+    # data splitting
+    X_train, X_val, y_train, y_val = train_test_split(data, label, random_state=0)
+
+    # training
+    model = LogisticRegression().fit(X_train, y_train)
+    
+    # prediction & evaluation
+    score = model.score(X_val, y_val)
+    print('Q2 accuracy: %.4f'%(score))
+    auc = roc_auc_score(y_val, model.predict_proba(X_val)[:, 1])
+    print('AUC: %.4f'%(auc))
+
+
+    if args.test is not None:
+        test_df = pd.read_csv(args.test)
+        test_data_ = generate_input(test_df, window_radius)
+        test_data = transformer.transform(test_data_)
+        predicted = model.predict_proba(test_data)[:, 1]
+
+        sequence_id_list    = []
+        residue_number_list = []
+        for _, item in test_df.iterrows():
+            sequence_id = item.sequence_id
+            sequence    = item.sequence
+            for i, aa in enumerate(sequence):
+                sequence_id_list.append(sequence_id)
+                residue_number_list.append(i+1) #0-origin to 1-origin
+
+        predicted_df = pd.DataFrame.from_dict({
+            "sequence_id": sequence_id_list,
+            "residue_number": residue_number_list,
+            "predicted_value": predicted,
+            })
+        predicted_df.to_csv(args.out, index=None)
+            
